8b1b5936d75c0805569eac0aba02c7bc8d7c5ccd
unknown <luccasdrapala@gmail.com>
2023-07-09 16:49:39 -0300

    Importando documentos

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+//my components
+import FirstComponent from './components/FirstComponent';
+import SecondComponent from './components/SecondComponent';
+
+//style
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Teste</h1>
+      <FirstComponent/>
+      <SecondComponent/>
     </div>
   );
 }
