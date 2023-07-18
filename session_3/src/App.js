0d2f10f061a92d9b57635918ddcc49500a6b3216
unknown <luccasdrapala@gmail.com>
2023-07-17 22:11:29 -0300

    REACT: imgs

--- session_3/src/App.js
+++ session_3/src/App.js
@@ -1,10 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ImgVariavel from './assets/IMG_3612.JPG'
+
 function App() {
   return (
     <div className="App">
       <h1>Teste</h1>
+
+      <div>
+        <img src="/IMG_3684.JPG" alt="Lula Wins" />
+      </div>
+
+      <div>
+        <img src={ImgVariavel} alt="imagem 2" />
+      </div>
     </div>
   );
 }
