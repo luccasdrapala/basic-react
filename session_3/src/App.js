4950dd423da75c97e92f550943e56870bee53f19
unknown <luccasdrapala@gmail.com>
2023-07-25 21:11:25 -0300

    REACT: estudos

--- session_3/src/App.js
+++ session_3/src/App.js
@@ -7,6 +7,7 @@ import ListRender from './components/ListRender';
 import ConditionalRender from './components/ConditionalRender';
 import ShowUserName from './components/ShowUserName';
 import CarDetails from './components/CarDetails';
+import Looping from './components/Looping';
 
 function App() {
 
@@ -20,7 +21,11 @@ function App() {
       <ConditionalRender/>
       <br />
       <ShowUserName name={lastName}/>
-      <CarDetails brand="ford" km={100.000} color="preto"/>
+      <CarDetails brand="ford" km={100.000} color="preto" newCar={true}/>
+      <CarDetails brand="VW" km={150.000} color="vermelho" newCar={true}/>
+      <CarDetails brand="fiat" km={300.000} color="branco" newCar={false}/>
+      <hr />
+      <Looping/>
     </div>
   );
 }
---
+++ session_3/src/components/Looping.js
@@ -0,0 +1,25 @@
+import CarDetails from "./CarDetails"
+
+const Looping = () => {
+
+    const cars = [
+        {id: 1, brand: "ford", km: 0, color: 'preto', newCar: true},
+        {id: 2, brand: "GM", km: 10.000, color: 'branco', newcar: false},
+        {id: 3, brand: "fiat", km: 100.000, color: 'vermelho', newcar: false},
+    ]
+
+  return (
+    <div>
+        {cars.map((car) => (
+            <CarDetails 
+                brand={car.brand} 
+                km={car.km} 
+                color={car.color} 
+                newCar={car.newCar}
+            />
+        ))}
+    </div>
+  )
+}
+
+export default Looping
