49522570ee7b4e222914f811fae7e9de2f04fa40
unknown <luccasdrapala@gmail.com>
2023-07-31 21:39:55 -0300

    REACT: CSS basicao chato

--- react_css/src/App.js
+++ react_css/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import MyComponents from './components/MyComponents';
 
 function App() {
   return (
     <div className="App">
       <h1>Opa</h1>
+
+      {/* CSS em componente */}
+      <MyComponents/>
+      <p>Paragrafo do app JS (vaza o CSS do componet)</p>
+
+      {/* INLINE CSS */}
+      <p style={{color: "blue", padding: "30px", backgroundColor: "greenyellow"}}>
+        CSS INLINE
+      </p>
+
     </div>
   );
 }
---
+++ react_css/src/components/MyComponents.js
@@ -0,0 +1,13 @@
+import "./MyComponents.css";
+
+const MyComponents = () => {
+  return (
+    <div>
+        <h1>Meu componente lindo</h1>
+        <p>Paragrafo do meu componente</p>
+        <p className="meu-paragrafo">Paragrafo com classe de CSS especifica</p>
+    </div>
+  )
+}
+
+export default MyComponents
