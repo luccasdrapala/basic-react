758376d4f0dfa5b9d11651fb64a944785561d893
unknown <luccasdrapala@gmail.com>
2023-08-08 21:49:57 -0300

    REACT: voltando

--- react_css/src/App.js
+++ react_css/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import MyComponents from './components/MyComponents';
 
 function App() {
+
+  const n = 15;
+  
   return (
     <div className="App">
       <h1>Opa</h1>
@@ -15,6 +18,9 @@ function App() {
       <p style={{color: "blue", padding: "30px", backgroundColor: "greenyellow"}}>
         CSS INLINE
       </p>
+
+      {/* CSS INLINE DINAMICO */}
+      <h2 style={n > 10 ? {backgroundColor: "green"} : {backgroundColor: "red"}}>CSS Dinamico</h2>
 
     </div>
   );
