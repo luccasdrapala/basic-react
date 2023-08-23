ffcbcb1ff3384c6d219b1899cd930bab2d9131af
unknown <luccasdrapala@gmail.com>
2023-08-22 21:15:37 -0300

    REACT: manipulando valores

--- forms/src/components/MyForm.js
+++ forms/src/components/MyForm.js
@@ -1,13 +1,28 @@
+import { useState } from 'react'
 import './MyForm.css'
 
 const MyForm = () => {
+
+  // genrenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
   return (
     <div>
         {/* 1- criacao do form */}
         <form>
             <div>
                 <label htmlFor="name">nome:</label>
-                <input type="text" name="name"/>
+                <input 
+                  type="text" 
+                  name="name"
+                  placeholder='Digit seu nome'
+                  onChange={handleName}
+                />
             </div>
 
             {/* label envolvendo input */}
