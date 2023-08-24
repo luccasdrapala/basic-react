abbaf797b6c8451519780f9392e843a1a5d73e67
unknown <luccasdrapala@gmail.com>
2023-08-24 20:33:00 -0300

    REACT: preparendo primeira pagina

---
+++ secret-word/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css';
+
+const StartScreen = () => {
+    return (
+        <div className='start'>
+            <h1>Secret Word</h1>
+            <p>Clique no botao abaixo para jogar</p>
+            <button type="button">JOGAR</button>
+        </div>
+    )
+}
+
+export default StartScreen;
