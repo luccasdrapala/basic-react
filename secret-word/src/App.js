43e6b71e82f958d7ec6d69258b11a060495f8f90
unknown <luccasdrapala@gmail.com>
2023-08-26 09:49:32 -0300

    REACT: criando e importando documentos no app.js

--- secret-word/src/App.js
+++ secret-word/src/App.js
@@ -1,12 +1,26 @@
-import './App.css';
-import StartScreen from './components/StartScreen';
+import './App.css'; // CSS
+import { useCallback, useEffect, useState } from "react"; // REACT
+import { wordsList } from "./data/words"; // DATA
+import StartScreen from './components/StartScreen'; // Components
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
+
+const stages = [
+    {id: 1, name: "start"},
+    {id: 2, name: "game"},
+    {id: 3, name: "end"}
+]
 
 function App() {
-  return (
-    <div className="App">
-    <StartScreen/>
-    </div>
-  );
+    const [gameStage, setGameStage] = useState(stages[0].name)
+
+    return (
+        <div className="App">
+            {gameStage === 'start' && <StartScreen />}
+            {gameStage === 'game' && <Game />}
+            {gameStage === 'end' && <GameOver />}            
+        </div>
+    );
 }
 
 export default App;
