f590edaaa3947775b42cd963f1121a551d7a880e
unknown <luccasdrapala@gmail.com>
2023-09-14 21:31:08 -0300

    REACT: requisicao do json server usando API fetch

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+
+	const [products, setProducts] = useState([]);
+
+	const url = 'http://localhost:3000/products';
+
+	//chamada assincrona
+	useEffect(() => {
+		async function fetchData() {
+			const res = await fetch(url);
+			const data = await res.json();
+
+		setProducts(data)
+		}
+		
+		fetchData()
+	}, []);
+
+	console.log(products);
+
+	return (
+    	<div className="App">
+      		<h1>Lista de produtos</h1>
+			<ul>
+				{products.map((product) => (
+					<li key={product.id}>{product.name} - {product.price}</li>
+				))}
+			</ul>
+    	</div>
+  	);
 }
 
 export default App;
