ad0be615bc8bb5c90b9b820b3dae35927c130d92
unknown <luccasdrapala@gmail.com>
2023-10-12 11:26:40 -0300

    REACT: 404 page

--- router/src/App.js
+++ router/src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
 import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 // componentes
 import Navbar from './components/Navbar';
@@ -23,6 +24,7 @@ function App() {
           <Route path='/about' element={<About />} />
           <Route path='/products/:id' element={<Product />} />
           <Route path='/products/:id/info' element={<Info />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </div>
