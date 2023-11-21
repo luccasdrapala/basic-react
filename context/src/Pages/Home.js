1a684951f1e25a92311d279577fdb30198faf909
Luccas Drapala <luccasdrapala@gmail.com>
2023-11-20 21:19:13 -0300

    REACT: antes tarde do que mais tarde

--- context/src/Pages/Home.js
+++ context/src/Pages/Home.js
@@ -7,12 +7,24 @@ import { useTitleColorContext } from '../hooks/useTitleColorContext';
 const Home = () => {
 	
 	const { counter } = useCounterContext();
-	const { color } = useTitleColorContext();
+	const { color, dispatch } = useTitleColorContext();
+
+	const setTitleColor = (color) => {
+		dispatch({type: color});
+	}
 
 	return (
 		<div>
 			<h1 style={{color: color}}>Home</h1>
 			<ChangeCounter />
+			<div>
+				<button onClick={() => setTitleColor("RED")}>
+					Vermelho
+				</button>
+				<button onClick={() => setTitleColor("BLUE")}>
+					Azul
+				</button>
+			</div>
 			<p>Valor do contador: {counter}</p>
 		</div>
 	)
