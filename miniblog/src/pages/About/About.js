5b0061017323f30d9d8bfcab1df0bc8c6529a69d
Luccas Drapala <luccasdrapala@gmail.com>
2023-11-23 20:44:39 -0300

    REACT-MINIBLOG: Criando Navbar, Footer e navegacao

---
+++ miniblog/src/pages/About/About.js
@@ -0,0 +1,9 @@
+import styles from "./About.modules.css";
+
+const About = () => {
+	return (
+		<h1>About</h1>
+	)
+}
+
+export default About
---
+++ miniblog/src/pages/Home/Home.js
@@ -0,0 +1,9 @@
+import styles from "./Home.modules.css";
+
+const Home = () => {
+	return (
+		<h1>Home</h1>
+	)
+}
+
+export default Home
