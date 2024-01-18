9dfc2ac5bbf73b09df811a2251da80c2b7eb78af
Luccas Drapala <luccasdrapala@gmail.com>
2024-01-17 21:14:07 -0300

    REACT-MINIBLOG: Tratando rotas para usuario nao logado e pagina 'Sobre'

--- miniblog/src/App.js
+++ miniblog/src/App.js
@@ -46,12 +46,29 @@ function App() {
 				<Navbar/>
 					<div className="container">
 						<Routes>
-							<Route path='/' element={<Home/>}/>
-							<Route path='/about' element={<About/>}/>
-							<Route path='/login' element={<Login/>}/>
-							<Route path='/register' element={<Register/>}/>
-							<Route path='/post/create' element={<CreatePost/>}/>
-							<Route path='/dashboard' element={<Dashboard/>}/>
+							<Route
+								path='/'
+								element={<Home/>}
+							/>
+							<Route 
+								path='/about'
+								element={<About/>}
+							/>
+							<Route 
+								path='/login'
+								element={!user ? <Login/> : <Navigate to="/"/>}
+							/>
+							<Route
+								path='/register'
+								element={!user ? <Register/> : <Navigate to="/"/>}
+							/>
+							<Route 
+								path='/post/create'
+								element={user ? <CreatePost/> : <Navigate to="/"/>}
+							/>
+							<Route 
+								path='/dashboard'
+								element={user ? <Dashboard/> : <Navigate to="/"/>}/>
 						</Routes>
 					</div>
 				<Footer/>
--- miniblog/src/pages/About/About.js
+++ miniblog/src/pages/About/About.js
@@ -1,9 +1,19 @@
-import styles from "./About.modules.css";
+import { Link } from "react-router-dom";
+import styles from "./About.module.css";
 
 const About = () => {
 	return (
-		<h1>About</h1>
+		<div className={styles.about}>
+			<h2>Sobre o Mini <span>Blog</span></h2>
+			<p>
+				Este Projeto consiste em um blog feito com 
+				react no front-end e Firebaseno back-end.
+			</p>
+			<Link to="/posts/create" className="btn">
+				Criar post
+			</Link>
+		</div>
 	)
 }
 
-export default About
+export default About;
