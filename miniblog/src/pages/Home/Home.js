a3d5b119e311f9e8d73b333abed3126d1fefefca
Luccas Drapala <luccasdrapala@gmail.com>
2024-02-12 20:54:46 -0300

    REACT-MINIBLOG: Estruturando esqueleto dos posts

--- miniblog/src/pages/Home/Home.js
+++ miniblog/src/pages/Home/Home.js
@@ -1,8 +1,37 @@
-import styles from "./Home.modules.css";
+import styles from "./Home.module.css";
+
+// hooks
+import {useNavigate, Link} from "react-router-dom";
+import {useState} from "react";
 
 const Home = () => {
+	const [query, setQuery] = useState("");
+	const [posts] = useState([]);
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	}
+
 	return (
-		<h1>Home</h1>
+		<div className={styles.home}>
+			<h1>Veja os postos mais recentes</h1>
+			<form onSubmit={handleSubmit} className={styles.searchForm}>
+				<input type="text" placeholder="Procurar posts" onChange={(e) => setQuery(e.target.value)}/>
+				<button className="btn btn-dark">Pesquisar</button>
+			</form>
+			<div>
+				<h1>Posts...</h1>
+				{posts && posts.length === 0 && (
+					<div className={styles.noPosts}>
+						<p>Nao foram encontrados posts</p>
+
+						<Link to={"/posts/create"} className="btn">
+							Criar posts
+						</Link>
+					</div>
+				)}
+			</div>
+		</div>
 	)
 }
 
