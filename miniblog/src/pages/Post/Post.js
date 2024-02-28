d466a576e8c6cb7b68eff65c094af5e0fe19ef55
Luccas Drapala <luccasdrapala@gmail.com>
2024-02-27 21:01:57 -0300

    REACT-MINIBLOG: Trazendo dados de um post especifico para o componente

--- miniblog/src/pages/Post/Post.js
+++ miniblog/src/pages/Post/Post.js
@@ -3,15 +3,23 @@ import styles from './Post.module.css';
 
 //hooks
 import {useParams} from 'react-router-dom';
+import {useFetchDocument} from '../../hooks/useFetchDocument';
 
 const Post = () => {
 	const {id} = useParams();
+	const {document, loading} = useFetchDocument('posts', id)
 
 	return (
 		<div>
-			POST
+			{loading && <p>Carregando post...</p>}
 			<h1>
-				{id}
+				{document && (
+					<>
+						<h1>
+							{document.title}
+						</h1>
+					</>
+				)}
 			</h1>
 		</div>
 	)
